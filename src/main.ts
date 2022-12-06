40adac13ad436ec904a2d9ddab67fa6bd05d8154
dreamcatch <zmswkfl@naver.com>
2022-12-06 20:43:38 +0900

    title : infinity scroll

---
+++ src/main.ts
@@ -0,0 +1,82 @@
+interface Movie{
+    Title : string
+    Year : string
+    imdbID : string
+    Type : string
+    Poster : string
+}
+
+const inputEl =  document.querySelector("input") as HTMLInputElement
+const buttonEl =  document.querySelector("button") as HTMLButtonElement
+const movieListEl =  document.querySelector(".movie-list") as HTMLDivElement
+const observerEl =  document.querySelector(".observer") as HTMLDivElement
+const loaderEl =  document.querySelector(".loader") as HTMLDivElement
+
+let currnetPage = 1
+let totalPage = 1
+
+
+inputEl.addEventListener("keydown", event =>{
+    if(event.key === "Enter" && ! event.isComposing){
+        searchMovies(inputEl.value)
+    }
+})
+buttonEl.addEventListener("click", ()=>{
+    searchMovies(inputEl.value)
+})
+
+const io = new IntersectionObserver(entries =>{
+    entries.forEach(entry =>{
+        if(currnetPage >= totalPage){
+            hideLoader()
+            return
+        }
+        if(entry.isIntersecting){
+            searchMovies(inputEl.value, currnetPage + 1)
+        }
+        
+    })
+})
+io.observe(observerEl)
+
+
+
+async function getMovies(title : string, page : number){
+    const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${title}&page=${page}`)
+    return await res.json()
+} 
+async function searchMovies(title : string, page = 1){
+    hideObserver()
+    const { Search, totalResults } = await getMovies(title, page)
+    if(page === 1){
+        movieListEl.innerHTML = ""
+        showLoader()
+    }
+    currnetPage = page
+    totalPage = Math.ceil(Number(totalResults) / 10)
+
+    Search.forEach((movie: Movie) => {
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie-item')
+        movieEl.innerHTML = /* HTML */ `
+          <div class="poster" style="background-image: url(${movie.Poster})"></div>
+          <h3>${movie.Title}</h3>
+        `
+        movieListEl.append(movieEl)
+      })
+    
+    showObserver()
+}
+
+function showObserver(){
+    observerEl.style.display="block"
+}
+function hideObserver(){
+    observerEl.style.display="none"
+}
+function showLoader(){
+    loaderEl.style.display="block"
+}
+function hideLoader(){
+    loaderEl.style.display="none"
+}
